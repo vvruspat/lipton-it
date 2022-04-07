fd1c70d060d926f368e5b773fb615f21172ad377
Artem Zhivokorentsev <a.zhivokorencev@corp.mail.ru>
2022-04-07 18:42:34 +0300

    add connection to database

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const config = require('config')
+const config = require('config');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
-const mainRouter = require('./routes/index')
+const mainRouter = require('./routes/index');
 
 const port = process.env.PORT || config.get('PORT');
 
+app.use(express.json())
+app.use(cors());
+app.options('*', cors());
+
 app.use('/api', mainRouter)
 
-app.listen(port, (err) => {
+app.listen(port, async (err) => {
     if (err) { 
-        console.error(err)
+        return console.error(err)
     }
-
-    console.log('API started at port', port);
+    try {
+        await mongoose.connect(`mongodb://dev:devsecret@45.8.249.80:27017/dev?authSource=admin`);
+        console.log('API started at port', port);
+    } catch (dbError) { 
+        console.log('Error db connection', dbError)
+    }
  })
--- routes/test.route.js
+++ routes/test.route.js
@@ -3,8 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/:testId', (req, res) => { 
-    console.log(req);
-    res.json({ testId: req.params.testId, testField: 'Heroku sucks' });
+    res.json({ testId: req.params.testId});
 })
 
 module.exports = router;
