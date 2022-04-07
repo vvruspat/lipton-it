683916622f1a25dda1d425c4e92b647624a14356
Artem Zhivokorentsev <a.zhivokorencev@corp.mail.ru>
2022-04-07 19:50:17 +0300

    add schema test

---
+++ shemas/test.schema.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const TestsSchema = new Schema({
+    testType: {
+        type: String, required: true, enum: ['five_sec', 'side_by_side', 'first_click']
+    },
+    title: String,
+    status: {
+        type: String, required: true, enum: ['available', 'completed', 'unavailable']
+    }
+});
+
+const Tests = model('Tests', TestSchema);
+
+module.exports = Tests;
