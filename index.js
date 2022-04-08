254450fc3c8319de5af6c72fc3e1a42bbd777b39
Artem Zhivokorentsev <a.zhivokorencev@corp.mail.ru>
2022-04-08 12:18:40 +0300

    add env params to bd connection

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const mainRouter = require('./routes/index');
 env.config();
 
 const port = process.env.PORT || config.get('PORT');
+const dbUser = proccess.env.DB_USER;
+const dbPass = proccess.env.DB_PWD;
+const dbHost = proccess.env.DB_HOST;
+const dbBase = proccess.env.DB_BASE;
 
 app.use(express.json())
 app.use(cors());
@@ -30,7 +34,7 @@ app.listen(port, async (err) => {
         return console.error(err)
     }
     try {
-        await mongoose.connect(`mongodb://dev:devsecret@45.8.249.80:27017/dev?authSource=admin`);
+        await mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:27017/${dbBase}?authSource=admin`);
         console.log('API started at port', port);
     } catch (dbError) {
         console.log('Error db connection', dbError);
